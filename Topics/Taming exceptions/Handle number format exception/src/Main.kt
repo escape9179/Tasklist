9f05dcf5725c2f52c494705fcbc077048365a193
escape9179 <tr3.logan@gmail.com>
2022-11-28 22:42:43 -0500

    Initial commit

--- Topics/Taming exceptions/Handle number format exception/src/Main.kt
+++
@@ -1,9 +0,0 @@
-fun parseCardNumber(cardNumber: String): Long {
-    val noSpaces = cardNumber.replace(" ", "")
-    val regex = Regex("(([0-9]){4}\\s){3}[0-9]{4}")
-    if (regex.matchEntire(cardNumber) == null) {
-        throw Exception()
-    } else {
-        return noSpaces.toLong()
-    }
-}
