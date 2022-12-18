336ca737dd8f35fabefd47888b5e06799e5e178c
escape9179 <tr3.logan@gmail.com>
2022-12-18 12:15:43 -0500

    Add edit, delete functionality

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -2,31 +2,51 @@ package tasklist
 
 import kotlinx.datetime.*
 import java.lang.NumberFormatException
-import java.time.LocalTime
-import kotlin.math.min
 import kotlin.system.exitProcess
-import kotlin.time.Duration
 
 val taskList = mutableListOf<Task>()
-var taskNumber = 1
 const val YEAR_LENGTH = 4
 const val MONTH_LENGTH_MAX = 2
 const val DAY_LENGTH_MAX = 2
+const val INTRODUCTION_LINE = "Input an action (add, print, edit, delete, end):"
+const val NO_TASKS_INPUT_MESSAGE = "No tasks have been input"
+const val INVALID_TASK_NUMBER_MESSAGE = "Invalid task number"
+val currentYear = Clock.System.now().toLocalDateTime(TimeZone.UTC).year
 
-data class Task(val date: LocalDate, val time: Time, val priority: String, val contents: MutableList<String> = mutableListOf()) {
-    val id = lastId
+data class Task(var priority: String, var date: LocalDate, var time: Time, var contents: MutableList<String> = mutableListOf()) {
+    var id = taskList.size + 1
     fun dateAsString(): String {
         return "${date.year}-${date.month}-${date.dayOfMonth}"
     }
 
-    companion object {
-        var lastId: Int = 1
+    fun getDueTag(): String {
+        val currentDate = Clock.System.now().toLocalDateTime(TimeZone.UTC).date
+        val daysUntil = currentDate.daysUntil(date)
+        return when {
+            daysUntil < 0 -> "O"
+            daysUntil > 0 -> "I"
+            else -> "T"
+        }
+    }
+
+    fun print() {
+        if (taskList.isEmpty()) {
+            println(NO_TASKS_INPUT_MESSAGE)
+            println(INTRODUCTION_LINE)
+            return
+        }
+
+            println("%-2d %s %s %s %s".format(id, date, time, priority, getDueTag()))
+            for (line in contents) {
+                println(line.padStart(line.length + 3))
+            }
+            println()
     }
 }
 
 data class Time(val hours: Int, val minutes: Int) {
     override fun toString(): String {
-        return "$hours:$minutes"
+        return "%02d:%02d".format(hours, minutes)
     }
 }
 
@@ -70,9 +90,8 @@ fun requestTime(): Time {
     val hours = parts[0].toInt()
     val minutes = parts[1].toInt()
 
-
     if (parts[0].isNotEmpty() && parts[0].length <= 2 && parts[1].isNotEmpty() && parts[1].length <= 2
-        && parts[0].toInt() <= 23) {
+        && parts[0].toInt() <= 23 && parts[1].toInt() < 60) {
         return Time(hours, minutes)
     }
     println("The input time is invalid")
@@ -80,54 +99,62 @@ fun requestTime(): Time {
 }
 
 fun requestPriority(): String {
+    println("Input the task priority (C, H, N, L):")
     val taskPriorities = arrayOf("C", "H", "N", "L")
     val priority = readln().uppercase()
     if (priority !in taskPriorities) {
-        println("Input the task priority (C, H, N, L):")
         return requestPriority()
     }
     return priority
 }
 
+fun requestTask(): MutableList<String> {
+    println("Input a new task (enter a blank line to end):")
+    val contents = mutableListOf<String>()
+    var input = readln().trim()
+    if (input.isBlank()) {
+        println("The task is blank")
+        println(INTRODUCTION_LINE)
+    }
+    while (input.isNotBlank()) {
+        contents.add(input)
+        input = readln().trim()
+    }
+    return contents
+}
+
 fun requestInput() {
     when (readln().lowercase()) {
         "add" -> {
-            println("Input the task priority (C, H, N, L):")
-            val priority = requestPriority()
-            val task = Task(requestDate(), requestTime(), priority)
+            val task = Task(requestPriority(), requestDate(), requestTime(), requestTask())
 
-            println("Input a new task (enter a blank line to end):")
-            var input = readln().trim()
-            if (input.isBlank()) {
-                println("The task is blank")
-                println("Input an action (add, print, end):")
-                return
-            }
-            while (input.isNotBlank()) {
-                task.contents.add(input)
-                input = readln().trim()
-            }
             taskList.add(task)
 //            taskNumber++
-            Task.lastId++
-            println("Input an action (add, print, end):")
+            println(INTRODUCTION_LINE)
             return
         }
 
         "print" -> {
             if (taskList.isEmpty()) {
-                println("No tasks have been input")
-                println("Input an action (add, print, end):")
+                println(NO_TASKS_INPUT_MESSAGE)
+                println(INTRODUCTION_LINE)
                 return
             }
-            // Output the task list, prepending a number before each task and using
-            // format specifiers to add spaces between the number and task.
-            taskList.forEach { task ->
-                println("%-2d %s %s %s".format(task.id, task.date, task.time, task.priority))
-                task.contents.forEach { println(it.padStart(it.length + 3))  }
-                println()
-            }
-            println("Input an action (add, print, end):")
+//
+//            // Output the task list, prepending a number before each task and using
+//            // format specifiers to add spaces between the number and task.
+//            taskList.forEach { task ->
+////                val taskContents = task.contents.joinToString("\n")
+//                println("%-2d %s %s %s %s".format(task.id, task.date, task.time, task.priority, task.getDueTag()))
+////                + "\n" + taskContents.padStart(taskContents.length + 3))
+//                for (line in task.contents) {
+//                    println(line.padStart(line.length + 3))
+//                }
+////                task.contents.forEach { println(it.padStart(it.length + 3))  }
+//                println()
+//            }
+            taskList.forEach { it.print() }
+            println(INTRODUCTION_LINE)
             return
         }
 
@@ -136,16 +163,96 @@ fun requestInput() {
             exitProcess(0)
         }
 
+        "delete" -> {
+            if (taskList.isEmpty()) {
+                println(NO_TASKS_INPUT_MESSAGE)
+                println(INTRODUCTION_LINE)
+                return
+            }
+            taskList.forEach { it.print() }
+//            println("Input the task number (1-${taskList.size}):")
+//            val input = readln()
+//            if (!isTaskNumber(input)) {
+//                println(INVALID_TASK_NUMBER_MESSAGE)
+//                requestTaskNumber()
+//                return
+//            }
+            val taskNumber = requestTaskNumber()
+            taskList.removeIf { it.id == taskNumber }
+            taskList.forEachIndexed { i, task -> task.id = i + 1 }
+            println("The task is deleted")
+            println(INTRODUCTION_LINE)
+        }
+
+        "edit" -> {
+            if (taskList.isEmpty()) {
+                println(NO_TASKS_INPUT_MESSAGE)
+                println(INTRODUCTION_LINE)
+                return
+            }
+            taskList.forEach { it.print() }
+            println()
+            val taskNumber = requestTaskNumber()
+            editTask(taskNumber)
+            println(INTRODUCTION_LINE)
+        }
+
         else -> {
             println("The input action is invalid")
-            println("Input an action (add, print, end):")
+            println(INTRODUCTION_LINE)
             requestInput()
         }
     }
 }
 
+fun editTask(taskNumber: Int) {
+    println("Input a field to edit (priority, date, time, task):")
+    when (readln().lowercase()) {
+        "priority" -> {
+            val priority = requestPriority()
+            taskList[taskNumber - 1].priority = priority
+        }
+        "date" -> {
+            val date = requestDate()
+            taskList[taskNumber - 1].date = date
+        }
+        "time" -> {
+            val time = requestTime()
+            taskList[taskNumber - 1].time = time
+        }
+        "task" -> {
+            val task = requestTask()
+            taskList[taskNumber - 1].contents = task
+        }
+        else -> {
+            println("Invalid field")
+            return editTask(taskNumber)
+        }
+    }
+    println("The task is changed")
+}
+
+fun isTaskNumber(number: String): Boolean {
+    return try {
+        0 < number.toInt() && number.toInt() <= taskList.size
+    } catch (e: NumberFormatException) {
+        false
+    }
+}
+
+fun requestTaskNumber(): Int {
+    println("Input the task number (1-${taskList.size}):")
+    val taskNumber = readln()
+    return if (isTaskNumber(taskNumber)) {
+        taskNumber.toInt()
+    } else {
+        println(INVALID_TASK_NUMBER_MESSAGE)
+        requestTaskNumber()
+    }
+}
+
 fun main() {
-    println("Input an action (add, print, edit, delete, end):")
+    println(INTRODUCTION_LINE)
     while (true) {
         requestInput()
     }
